d8fb7bb2b34e27c0a41eab4f194fad5084c0c31e
Marcin Zimecki <marcin.zimecki@lhsystems.com>
2020-05-22 12:29:36 +0200

    refactoring

--- MazeGenerator.py
+++ MazeGenerator.py
@@ -3,10 +3,10 @@ import random
 import time
 from collections import deque
 
-WIDTH = 440
-HEIGHT = 440
+WIDTH = 640
+HEIGHT = 640
 FPS = 30
-DELAY = 0.01
+DELAY = 0.02
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -18,70 +18,84 @@ BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
+CELL_SIZE = 20
+ROWS = 30
+COLS = 30
+
 grid = []
 stack = deque()
 visited = []
 solution = {}
-w = 20
 
 
 def make_grid(x, y):
     init_x = x
-    for i in range(1, 21):
+    for i in range(0, ROWS):
         x = init_x
-        y += 20
-        for j in range(1, 21):
-            pygame.draw.line(screen, WHITE, [x, y], [x + w, y])  # up
-            pygame.draw.line(screen, WHITE, [x, y], [x, y + w])  # left
-            pygame.draw.line(screen, WHITE, [x, y + w], [x + w, y + w])  # down
-            pygame.draw.line(screen, WHITE, [x + w, y], [x + w, y + w])  # right
+        y += CELL_SIZE
+        for j in range(0, COLS):
+            pygame.draw.line(screen, WHITE, [x, y], [x + CELL_SIZE, y])  # up
+            pygame.draw.line(screen, WHITE, [x, y], [x, y + CELL_SIZE])  # left
+            pygame.draw.line(screen, WHITE, [x, y + CELL_SIZE], [x + CELL_SIZE, y + CELL_SIZE])  # down
+            pygame.draw.line(screen, WHITE, [x + CELL_SIZE, y], [x + CELL_SIZE, y + CELL_SIZE])  # right
             grid.append((x, y))
-            x = x + 20
+            x = x + CELL_SIZE
     pygame.display.update()
 
 
 def mark_solution_cell(x, y, color):
-    pygame.draw.rect(screen, color, [x + 9, y + 9, w - 18, w - 18])
+    pygame.draw.rect(screen, color, [x + CELL_SIZE/2, y + CELL_SIZE/2, 2, 2])
     pygame.display.update()
 
 
 def mark_cell(x, y, color):
-    pygame.draw.rect(screen, color, [x + 1, y + 1, w - 1, w - 1])
+    pygame.draw.rect(screen, color, [x + 1, y + 1, CELL_SIZE - 1, CELL_SIZE - 1])
     pygame.display.update()
 
 
 def go_up(x, y):
-    pygame.draw.rect(screen, BLUE, [x + 1, y - w + 1, w - 1, 2*w - 1])
+    pygame.draw.rect(screen, BLUE, [x + 1, y - CELL_SIZE + 1, CELL_SIZE - 1, 2*CELL_SIZE - 1])
     pygame.display.update()
 
 
 def go_down(x, y):
-    pygame.draw.rect(screen, BLUE, [x + 1, y + 1, w - 1, 2*w - 1])
+    pygame.draw.rect(screen, BLUE, [x + 1, y + 1, CELL_SIZE - 1, 2*CELL_SIZE - 1])
     pygame.display.update()
 
 
 def go_right(x, y):
-    pygame.draw.rect(screen, BLUE, [x + 1, y + 1, 2*w - 1, w - 1])
+    pygame.draw.rect(screen, BLUE, [x + 1, y + 1, 2*CELL_SIZE - 1, CELL_SIZE - 1])
     pygame.display.update()
 
 
 def go_left(x, y):
-    pygame.draw.rect(screen, BLUE, [x - w + 1, y + 1, 2*w - 1, w - 1])
+    pygame.draw.rect(screen, BLUE, [x - CELL_SIZE + 1, y + 1, 2*CELL_SIZE - 1, CELL_SIZE - 1])
     pygame.display.update()
 
 
 def get_unvisited_neighbors(x, y):
     neighbors = []
-    if (x + w, y) in grid and (x + w, y) not in visited:  # right neighbor
-        neighbors.append((x + w, y))
-    if ((x, y + w)) in grid and (x, y + w) not in visited:  # down neighbor
-        neighbors.append((x, y + w))
-    if (x - w, y) in grid and (x - w, y) not in visited:  # left neighbor
-        neighbors.append((x - w, y))
-    if (x, y - w) in grid and (x, y - w) not in visited:  # up neighbor
-        neighbors.append((x, y - w))
+    if (x + CELL_SIZE, y) in grid and (x + CELL_SIZE, y) not in visited:  # right neighbor
+        neighbors.append((x + CELL_SIZE, y))
+    if ((x, y + CELL_SIZE)) in grid and (x, y + CELL_SIZE) not in visited:  # down neighbor
+        neighbors.append((x, y + CELL_SIZE))
+    if (x - CELL_SIZE, y) in grid and (x - CELL_SIZE, y) not in visited:  # left neighbor
+        neighbors.append((x - CELL_SIZE, y))
+    if (x, y - CELL_SIZE) in grid and (x, y - CELL_SIZE) not in visited:  # up neighbor
+        neighbors.append((x, y - CELL_SIZE))
     return neighbors
-    
+
+
+def remove_walls_between_cells(x1, y1, x2, y2):
+     if x1 == x2 and y2 == y1 + CELL_SIZE:
+        go_down(x1, y1)
+     if x1 == x2 and y2 == y1 - CELL_SIZE:
+        go_up(x1, y1)
+     if y1 == y2 and x2 == x1 + CELL_SIZE:
+        go_right(x1, y1)
+     if y1 == y2 and x2 == x1 - CELL_SIZE:
+        go_left(x1, y1)
+
 
 def generate_maze(x, y):
     # Choose the initial cell, mark it as visited and push it to the stack
@@ -104,14 +118,7 @@ def generate_maze(x, y):
             (next_cell_x, next_cell_y) = neighbors[next_neighbor_index]
             
             # Remove the wall between the current cell and the chosen cell
-            if current_cell_x == next_cell_x and next_cell_y == current_cell_y + w:
-                go_down(current_cell_x, current_cell_y)
-            if current_cell_x == next_cell_x and next_cell_y == current_cell_y - w:
-                go_up(current_cell_x, current_cell_y)
-            if current_cell_y == next_cell_y and next_cell_x == current_cell_x + w:
-                go_right(current_cell_x, current_cell_y)
-            if current_cell_y == next_cell_y and next_cell_x == current_cell_x - w:
-                go_left(current_cell_x, current_cell_y)
+            remove_walls_between_cells(current_cell_x, current_cell_y, next_cell_x, next_cell_y)
 
             # Mark the chosen cell as visited and push it to the stack
             visited.append((next_cell_x, next_cell_y))
@@ -135,9 +142,9 @@ def show_solution(x, y):
         time.sleep(DELAY)
 
 
-make_grid(20, 0)
-generate_maze(20, 20)
-show_solution(400, 400)
+make_grid(CELL_SIZE, 0)
+generate_maze(CELL_SIZE, CELL_SIZE)
+show_solution(ROWS * CELL_SIZE, COLS * CELL_SIZE)  # solution starting from the last cell (bottom right corner)
 
 running = True
 while running:
